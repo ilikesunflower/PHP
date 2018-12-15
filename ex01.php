ab9483b4d46bb5635df088e7b9a68d5bdd727f5c
= <=>
2018-12-15 01:24:03 -0800

    first commit

---
+++ ex01.php
@@ -0,0 +1,12 @@
+
+       <?php
+       $xau="vŨ vĂN tHƯoNg";
+        //chuyển đổi tất cả các chữ cái trong chuỗi thành chữ hoa  
+		print(strtoupper($xau))."<br>";  
+		//chuyển đổi tất cả các chữ cái trong chuỗi thành chữ thường 
+		print(strtolower("BÀi TaP PhP CO BAn VA NANG CAO."))."<br>";  
+		//chuyển đổi chữ cái đầu tiên của chuỗi thành chữ hoa 
+		print(ucfirst("bai tap php co ban va nang cao."))."<br>";  
+		//chuyển đổi chữ cái đầu tiên của tất cả các từ trong chuỗi thành chữ hoa
+		print(ucwords("bai tap php co ban va nang cao."))."<br>";
+       ?>
